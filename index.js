b6ff181518d321cbdeec59d4f4117bf62eb13b49
aduh95 <duhamelantoine1995@gmail.com>
2017-11-15 17:23:44 +0100

    Remove debug log from index file
    I am using your library into my program and at each run, it tests whatever the argument I provide for my program and outputs fail messages on the console.
    
    I suggest you to move these lines onto a separate module.

--- index.js
+++ index.js
@@ -41,15 +41,4 @@ var isPortAvailable = function(port){
 // Last Error track
 isPortAvailable.lastError = '';
 
-
-// Allow argument from command line, for testing purposes
-if(typeof process.argv[2] !== 'undefined'){
-	var port = process.argv[2];
-	console.log('Checking port ' + port + ' ...');
-	isPortAvailable( port ).then(function(status){
-		if(status) console.log('Port ' + port + ' is available!');
-		else  console.log('Port ' + port + ' is  NOT available! ('+ isPortAvailable.lastError +')');
-	});
-}
-
 module.exports = isPortAvailable;
